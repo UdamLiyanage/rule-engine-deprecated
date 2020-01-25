650fde757266ffdcce083704f1840deb3d6994a5
Udam Liyanage <udam1998@gmail.com>
2020-01-25 18:45:32 +0530

    GIN Router setup

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"github.com/arangodb/go-driver"
+	"github.com/gin-gonic/gin"
 )
 
 var database driver.Database
 
 func init() {
 	databaseConnect()
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.POST("/mqtt")
+
+	return r
 }
 
 func main() {

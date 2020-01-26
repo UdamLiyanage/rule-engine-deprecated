583f2d7666f94263728ad11e4ed79419cc3a5a56
Udam Liyanage <udam1998@gmail.com>
2020-01-26 17:26:16 +0530

    Removed test cases

--- engine_test.go
+++ engine_test.go
@@ -1,7 +1,1 @@
 package main
-
-import "testing"
-
-func TestInferRules(t *testing.T) {
-	testRequestStatusCode("POST", "/mqtt-broker", nil, 200, t)
-}
--- main_test.go
+++ main_test.go
@@ -5,10 +5,6 @@ import (
 	"testing"
 )
 
-func TestMQTTRoute(t *testing.T) {
-	testRequestStatusCode("POST", "/mqtt-broker", nil, http.StatusOK, t)
-}
-
 func TestInvalidRoute(t *testing.T) {
 	testRequestStatusCode("POST", "/mqtt", nil, http.StatusNotFound, t)
 }
